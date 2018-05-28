14b5ae530c000bc65d53a744530761083166f261
BhattGi <39691350+BhattGi@users.noreply.github.com>
2018-05-28 00:58:21 -0700

    commit by gi

--- TestClass.java
+++ TestClass.java
@@ -1,11 +1,11 @@
 public class TestClass{
 	
 	public static void main(String args[]){
-		System.out.println("Hello prj1");
-		System.out.println("Hello prj2");
-		System.out.println("Hello prj3");
-		System.out.println("Hello prj4");
-		System.out.println("Hello prj5");
+		System.out.println("Hello prj1");;
+		System.out.println("Hello prj2");;
+		System.out.println("Hello prj3");;
+		System.out.println("Hello prj4");;
+		System.out.println("Hello prj5");;
 	} 
 	
 }
