e4cfc4bbc7ec996b86ad59d8e6435c70b89c8da4
BhattGi <39691350+BhattGi@users.noreply.github.com>
2018-05-28 00:57:51 -0700

    Update TestClass.java

--- TestClass.java
+++ TestClass.java
@@ -2,6 +2,10 @@ public class TestClass{
 	
 	public static void main(String args[]){
 		System.out.println("Hello prj1");
+		System.out.println("Hello prj2");
+		System.out.println("Hello prj3");
+		System.out.println("Hello prj4");
+		System.out.println("Hello prj5");
 	} 
 	
 }
